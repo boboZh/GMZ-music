f24fa6d8c938cd8d7e3058fff901dda75096aed5
gewei <1242110220@qq.com>
2018-05-02 17:01:51 +0800

    我的

--- src/components/mine/mine.vue
+++ src/components/mine/mine.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mine">
     <ul>
-      <li>本地音乐（<span>12</span>）</li>
-      <li>最近播放（<span>100</span>）</li>
-      <li>下载管理（<span>22</span>）</li>
-      <li>我的电台（<span>0</span>）</li>
-      <li>我的收藏（专辑/歌手/视频/专栏）</li>
+      <li>本地音乐<span>（12）</span></li>
+      <li>最近播放<span>（100）</span></li>
+      <li>下载管理<span>（22）</span></li>
+      <li>我的电台<span>（0）</span></li>
+      <li>我的收藏<span>（专辑/歌手/视频/专栏）</span></li>
     </ul>
   </div>
 </template>
@@ -21,4 +21,11 @@ export default {
 @import "../../common/style/mixin.sass"
 .mine
   margin-top: 90px
+  li
+   height: 40px
+   line-height: 40px
+   margin: 0 20px
+   border-bottom: 1px solid #ddd
+   span
+    color: #7e8c8d
 </style>
